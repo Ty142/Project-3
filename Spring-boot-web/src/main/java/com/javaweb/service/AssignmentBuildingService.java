9962b0098db7dde4517f084e48a48d6f6c70a238
Ty142 <tranduykhang159@gmail.com>
2024-08-27 21:24:57 +0700

    check

--- Spring-boot-web/src/main/java/com/javaweb/service/AssignmentBuildingService.java
+++
@@ -1,9 +0,0 @@
-package com.javaweb.service;
-
-import com.javaweb.entity.BuildingEntity;
-
-import java.util.List;
-
-public interface AssignmentBuildingService {
-    void deleteByBuildingIdIn(List<Long> ids);
-}
